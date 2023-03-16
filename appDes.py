7a82726bb4499673bc1d24af5b18c09a052437a5
Chí Hiếu <hieumeo171@gmail.com>
2023-03-16 22:40:36 +0700

    Create appDes.py

---
+++ appDes.py
@@ -0,0 +1,31 @@
+description = """
+Bài tập lớn môn **Lập trình python**
+## Thành viên nhóm
+* **A38253 Nguyễn Hoàng Đức Anh**
+* **A38520 Mai Văn Mạnh**
+* **A38911 Vũ Tiến Dũng**
+* **A41174 Hoàng Chí Hiếu** (_Trưởng nhóm_).
+"""
+tags_metadata = [
+    {
+        "name" : "Trang chủ",
+        "description" : "Hiển thị thông tin của nhóm và thông tin thành viên"
+    },
+    {
+        "name" : "Duc anh np",
+        "description" : "Cập nhập thông tin Điểm trung bình và lấy điểm tổng kết của học sinh"
+    },
+    {
+        "name" : "Duc anh pd",
+        "description" : "Thống kê điểm của môn theo lớp và Tìm kiếm học sinh có thông tin"
+    }
+]
+descriptionApi = {
+    "DucAnhPd": {
+        "ThongKeMonHoc" : "Nhập mã môn học để nhận lại thông tin trả về là 1 bảng các lớp học có số điểm của môn học. subjectid mang giá trị số nguyên dương",
+        "TimKiemHocSinh": "Nhập một trong các thoomg tin cá nhân của học sinh để nhận lại thông tin của bảng chứa các thông tin cần tìm"
+    },
+    "DucANhNp" : {
+        
+    }
+}
